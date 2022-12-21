5dc375c4f7d5e3e6c4d7c903c94af40688a4299a
Omkar Pathak <92076273+OmkarPathak1@users.noreply.github.com>
2022-12-21 13:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
-from turtle import Turtle
-ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-class Scoreboard(Turtle):
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-    def __init__(self):
-        super().__init__()
-        self.score = 0
-        with open("data.txt") as data:
-            self.high_score = int(data.read())
-        self.color("white")
-        self.penup()
-        self.goto(0, 270)
-        self.hideturtle()
-        self.update_scoreboard()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-    def update_scoreboard(self):
-        self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
-        self.score = 0
-        self.update_scoreboard()
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-    def increase_score(self):
-        self.score += 1
-        self.update_scoreboard()
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
